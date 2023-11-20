9c3b802a8e311afc52381fa21f00f4f085f668eb
youngrong.hwang <youngrong.hwang@hyosung.com>
2023-11-20 20:48:26 +0900

    modal

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -12,6 +12,37 @@ export default function MovieModal({
     setModalOpen
 }) {
   return (
-    <div>index</div>
+    <div className='presentation'>
+        <div className='wrapper-modal'>
+            <div className='modal'>
+                <span onClick={() => setModalOpen(false)} className='modal-close'>
+                    x
+                </span>
+                <img
+                    className='modal_poster-img'
+                    src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+                    alt='modal_poster-img' 
+                />
+
+                <div className='modal_content'>
+                    <p className='modal_details'>
+                        <span className='modal_user_perc'>
+                            100% for you
+                        </span>
+                        {release_date ? release_date : first_air_date}
+                    </p>
+                    <h2 className='modal_title'>
+                        {title ? title : name}
+                    </h2>
+                    <p className='modal_overview'>
+                        평점: {vote_average}
+                    </p>
+                    <p className='modal_overview'>
+                        {overview ? overview : ''}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
